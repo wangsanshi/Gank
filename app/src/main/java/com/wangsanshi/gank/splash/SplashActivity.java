0df77392991134daf6c4381cd4d34537473eabd2
王磊 <forwlwork@gmail.com>
2016-12-16 18:06:29 +0800

    frist commit

---
+++ app/src/main/java/com/wangsanshi/gank/splash/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.wangsanshi.gank.splash;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.wangsanshi.gank.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+
+}
